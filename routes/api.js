a908dcd0a5773ddcff627dc479d0133eb26ce385
Alec74 <alecmcglone@gmail.com>
2021-07-29 19:37:58 -0400

    routes/api.js

---
+++ routes/api.js
@@ -0,0 +1,69 @@
+const { response } = require("express");
+const mongoose = require("mongoose");
+const router = require("express").Router();
+const Workout = require("../models/workout");
+
+router.post("/api/workouts", (req, res) => {
+    Workout.create({})
+        .then((workout) => {
+            res.json(workout)
+        })
+        .catch((err) => {
+            res.json(err)
+        })
+});
+
+router.get("/api/workouts", (req, res) => {
+    Workout.aggregate([
+        {
+            $addFields: {
+                totalDuration: { $sum: "$exercises.duration" }
+            }
+        },
+        {
+            $sort: { "day": 1 }
+        }
+    ])
+        .then(workout => {
+            res.json(workout);
+        })
+        .catch((err) => {
+            res.json(err)
+        });
+});
+
+router.get("/api/workouts/range", (req, res) => {
+    Workout.aggregate([
+        {
+            $addFields: {
+                totalDuration:
+                    { $sum: "exercises.duration" },
+                totalweight:
+                    { $sum: "exercises.weight" }
+            }
+        }
+    ])
+        .limit(7)
+        .then((workout) => {
+            res.json(workout)
+        })
+        .catch((err) => {
+            res.json(err)
+        });
+})
+
+router.put("/api/workouts/:id", (req, res) => {
+    Workout.findOneAndUpdate(
+        req.params.id,
+        { $push: { exercise: req.body } },
+        { new: true, runValidators: true }
+    )
+        .then(workout => {
+            res.json(workout);
+        })
+        .catch((err) => {
+            res.json(err)
+        });
+})
+
+module.exports = router;
